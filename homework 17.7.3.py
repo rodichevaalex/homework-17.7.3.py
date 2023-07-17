6f4a75bb5fa421272efa4b12e09d390e24ee6f45
rodichevaalex <130316452+rodichevaalex@users.noreply.github.com>
2023-07-18 01:46:32 +0300

    Add files via upload

---
+++ homework 17.7.3.py
@@ -0,0 +1,9 @@
+deposit = []
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму, которую планируете положить под проценты: "))
+deposit.append(int(per_cent['ТКБ'] * money / 100))
+deposit.append(int(per_cent['СКБ'] * money / 100))
+deposit.append(int(per_cent['ВТБ'] * money / 100))
+deposit.append(int(per_cent['СБЕР'] * money / 100))
+print("Накопленные средства за год вклада в каждом из банков =",deposit)
+print("Максимальная сумма, которую вы можете заработать =",max(deposit))
